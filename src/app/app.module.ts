0e6cc5fd8cafd7bc59b1c8838b101861d37ce623
Kaopuk <baptiste.lavignon@gmail.com>
2018-10-04 11:28:20 +0200

    creez des pages et naviguez

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AppareilsPage } from '../pages/appareils/appareils';
+import {SingleAppareilPage} from "../pages/appareils/single-appareil/single-appareil";
+import {SettingsPage} from "../pages/settings/settings";
+import {TabsPage} from "../pages/settings/tabs/tabs";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AppareilsPage,
+    SingleAppareilPage,
+    SettingsPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AppareilsPage,
+    SingleAppareilPage,
+    SettingsPage,
+    TabsPage
+
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
