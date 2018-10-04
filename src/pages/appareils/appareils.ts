3d266f6516f1f49bb02d85fb2dc925afe4b46452
Kaopuk <baptiste.lavignon@gmail.com>
2018-10-04 13:58:55 +0200

    Utilisez des components Ionic en cours

--- src/pages/appareils/appareils.ts
+++ src/pages/appareils/appareils.ts
@@ -9,10 +9,35 @@ import {SingleAppareilPage} from "./single-appareil/single-appareil";
 
 export class AppareilsPage{
 
+  appareilsList = [
+    {
+      name: 'Machine à laver',
+      description: [
+        'Volume : 6 litres',
+        'Temps de lavage : 2 heures',
+        'Consommation : 173kwh/an'
+      ]
+    },
+    {
+      name: 'Télévision',
+      description: [
+        'Dimension : 40 pouces',
+        'Consommation : 22kwh/an'
+      ]
+    },
+    {
+      name: 'Ordinateur',
+      description: [
+        'Marque : fait maison',
+        'Consommation  : 500 kwh/an'
+      ]
+    }
+  ];
+
     constructor(private navCtrl: NavController) {
     }
 
-    onLoadAppareil(name: String) {
-      this.navCtrl.push(SingleAppareilPage, {appareilName: name});
+    onLoadAppareil(appareil: {name: string, description: string[]}) {
+      this.navCtrl.push(SingleAppareilPage, {appareil: appareil});
     }
 }
