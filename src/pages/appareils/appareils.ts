2a33d3574fda139e5df6d434c2a7b120554b646c
Kaopuk <baptiste.lavignon@gmail.com>
2018-10-05 09:35:24 +0200

    Utilisez des components Ionic

--- src/pages/appareils/appareils.ts
+++ src/pages/appareils/appareils.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {NavController} from "ionic-angular";
 import {SingleAppareilPage} from "./single-appareil/single-appareil";
+import {ModalController} from "ionic-angular";
+
 
 @Component({
   selector: 'page-appareils',
@@ -34,10 +35,11 @@ export class AppareilsPage{
     }
   ];
 
-    constructor(private navCtrl: NavController) {
+    constructor(private modalCtrl: ModalController) {
     }
 
     onLoadAppareil(appareil: {name: string, description: string[]}) {
-      this.navCtrl.push(SingleAppareilPage, {appareil: appareil});
+      let modal = this.modalCtrl.create(SingleAppareilPage, {appareil: appareil});
+      modal.present();
     }
 }
