57f95b747ea54b23ca5a54b83794bb7ab6f66f99
Kaopuk <baptiste.lavignon@gmail.com>
2018-10-09 14:45:52 +0200

    Intégrez un backend - création et authentification : - Créez un backend Firebase - Authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import {Component, ViewChild} from '@angular/core';
 import {MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import * as firebase from 'firebase';
 
 import {TabsPage} from "../pages/settings/tabs/tabs";
 import {OptionsPage} from "../pages/options/options";
+import {AuthPage} from "../pages/auth/auth";
 @Component({
   templateUrl: 'app.html'
 })
@@ -13,6 +14,8 @@ export class MyApp {
 
   tabsPage: any = TabsPage;
   optionsPage: any = OptionsPage;
+  authPage: any = AuthPage;
+  isAuth: boolean;
   @ViewChild('content') content: NavController;
 
   constructor(platform: Platform,
@@ -20,15 +23,41 @@ export class MyApp {
               splashScreen: SplashScreen,
               private menuCtrl: MenuController) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+
+      let config = {
+        apiKey: "AIzaSyB3_1Tq6rQ6ZC5nrVbvEZ4iLiLtZyZKav0",
+        authDomain: "cours-ionic.firebaseapp.com",
+        databaseURL: "https://cours-ionic.firebaseio.com",
+        projectId: "cours-ionic",
+        storageBucket: "cours-ionic.appspot.com",
+        messagingSenderId: "519836165414"
+      };
+      firebase.initializeApp(config);
+
+      firebase.auth().onAuthStateChanged(
+        (user) =>{
+          if(user) {
+            this.isAuth = true;
+            this.content.setRoot(TabsPage);
+          } else {
+            this.isAuth = false;
+            this.content.setRoot(AuthPage, {mode: 'connect'});
+          }
+        }
+      );
+
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 
-  onNavigate(page: any) {
-    this.content.setRoot(page);
+  onNavigate(page: any, data?: {}) {
+    this.content.setRoot(page, data ? data : null);
+    this.menuCtrl.close();
+  }
+
+  onDisconnect() {
+    firebase.auth().signOut();
     this.menuCtrl.close();
   }
 }
